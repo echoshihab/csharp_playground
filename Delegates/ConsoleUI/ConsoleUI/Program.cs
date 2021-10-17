271bbf61e06cf0a3b2fae6893a41860372e1adc9
echoshihab <echoshihab@gmail.com>
2021-10-17 11:25:51 -0400

    set up pre delegate functionalities

---
+++ Delegates/ConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        private static ShoppingCartModel _cart;
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is {_cart.GenerateTotal():C2}");
+            Console.WriteLine("Please press any key to exit the application");
+            Console.ReadKey();
+
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            _cart.Items.Add(new ProductModel{ItemName = "Cereal", Price = 3.63M});
+            _cart.Items.Add(new ProductModel{ItemName = "Milk", Price = 2.95M});
+            _cart.Items.Add(new ProductModel{ItemName = "Strawberries", Price = 7.51M});
+            _cart.Items.Add(new ProductModel{ItemName = "Blueberries", Price = 8.84M});
+
+        }
+    }
+}
---
+++ Delegates/ConsoleUI/DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+
+
+        public List<ProductModel> Items { get; set; } 
+
+        public decimal GenerateTotal()
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            return subTotal switch
+            {
+                > 100 => subTotal * 0.80M,
+                > 50 => subTotal * 0.85M,
+                > 10 => subTotal * 0.90M,
+                _ => subTotal
+            };
+        }
+    }
+}
