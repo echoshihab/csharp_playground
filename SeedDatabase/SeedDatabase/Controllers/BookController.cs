da9fb76a5fbb63d60b09273ac79db0a93751984d
echoshihab <echoshihab@gmail.com>
2021-12-05 17:54:03 -0500

    display seed data in views

--- SeedDatabase/SeedDatabase/Controllers/BookController.cs
+++ SeedDatabase/SeedDatabase/Controllers/BookController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SeedDatabase.Models;
+using SeedDatabase.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,23 +14,24 @@ namespace SeedDatabase.Controllers
 {
     public class BookController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
-
-        public BookController()
+        public BookController(ApplicationDbContext context)
         {
-
+            _context = context;
         }
 
         public IActionResult Index()
         {
-
-            return View();
+            var books = _context.Books.Include(b => b.Author);
+            return View(books);
         }
 
 
         public IActionResult Details(Guid id)
         {
-            return View();
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
+            return View(book);
         }
 
     }
