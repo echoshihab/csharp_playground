7f2131ce76a0fac537a1397cf2da597cce060ff0
Shihab Khan <echoshihab@gmail.com>
2023-02-06 21:25:05 -0500

    add lambda expression converter

--- Delegates/ConsoleUI/ConsoleUI/Program.cs
+++ Delegates/ConsoleUI/ConsoleUI/Program.cs
@@ -1,54 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using DemoLibrary;
 
 namespace ConsoleUI
 {
     class Program
     {
-        private static ShoppingCartModel _cart = new ShoppingCartModel();
+       
 
-        
         static void Main(string[] args)
         {
-            PopulateCartWithDemoData();
+            Manager accountingVP = new Manager();
 
-            Console.WriteLine($"The Discounted total for the cart is {_cart.GenerateTotal(ShowTotalBeforeDiscount, CalculateDiscount, AlertMethodCompletion):C2}");
-            Console.WriteLine("Please press any key to exit the application");
-            Console.ReadKey();
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
 
-        }
+            Employee emp = new Employee();
 
-        private static void AlertMethodCompletion(string message)
-        {
-            Console.WriteLine(message);
-        }
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
 
-        //this just has to match the method signature of delegate method declared in shopping cart model
-        private static void ShowTotalBeforeDiscount(decimal total)
-        {
-            Console.WriteLine($"Total before discount: {total:C2}");
-        }
+            Console.WriteLine($"{emp.FirstName}'s salary is ${emp.Salary}/hour");
 
-        private static void PopulateCartWithDemoData()
-        {
-            _cart.Items.Add(new ProductModel{ItemName = "Cereal", Price = 3.63M});
-            _cart.Items.Add(new ProductModel{ItemName = "Milk", Price = 2.95M});
-            _cart.Items.Add(new ProductModel{ItemName = "Strawberries", Price = 7.51M});
-            _cart.Items.Add(new ProductModel{ItemName = "Blueberries", Price = 8.84M});
-
+            Console.ReadLine();
         }
 
 
-        private static decimal CalculateDiscount(List<ProductModel> items, decimal subTotal)
-        {
-            return subTotal switch
-            {
-                > 100 => subTotal * 0.80M,
-                > 50 => subTotal * 0.85M,
-                > 10 => subTotal * 0.90M,
-                _ => subTotal
-            };
-        }
+       
     }
 }
---
+++ LambdaExpressionConverter/Program.cs
@@ -0,0 +1,148 @@
+﻿
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+
+public class PatientDto
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
+
+public class Patient
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string HealthCard { get;set;}
+    public string VersionCode { get;set;}
+
+}
+
+
+public class Student
+{
+    public string ClassName { get;set;}
+
+    public Guid StudentId { get;set;}
+
+}
+
+public class StudentDto
+{
+    public string ClassName { get; set; }
+
+    public Guid StudentId { get; set; }
+
+}
+
+
+public class LamdbaExpressionConverter<TSource, TTarget>: ExpressionVisitor
+{
+    private ReadOnlyCollection<ParameterExpression> _parameterExpressions;
+
+    public Expression<Func<TTarget, bool>> Convert(Expression expression)
+    {
+        return (Expression<Func<TTarget, bool>>)Visit(expression);
+    }
+
+    protected override Expression VisitLambda<T>(Expression<T> node)
+    {
+
+        _parameterExpressions = VisitAndConvert<ParameterExpression>(node.Parameters, "LamdbaExpressionConverter");
+
+        return Expression.Lambda(Visit(node.Body), _parameterExpressions);
+    }
+
+    protected override Expression VisitParameter(ParameterExpression node)
+    {
+
+        if (node.Type == typeof(TSource))
+        {
+
+            var parameterExpression = _parameterExpressions?.FirstOrDefault(p => p.Name == node.Name);
+            if (parameterExpression == null)
+            {
+                if (node.Type == typeof(TSource))
+                {
+                    return Expression.Parameter(typeof(TTarget), node.Name);
+                }
+                else
+                {
+                    return node;
+                }
+            } else 
+                return parameterExpression;
+
+        }
+      
+        return base.VisitParameter(node);
+    }
+
+    protected override Expression VisitMember(MemberExpression node)
+    {
+        if(node.Member.DeclaringType == typeof(TSource) && typeof(TTarget).GetProperty(node.Member.Name) != null)
+        {
+
+            return Expression.MakeMemberAccess(Visit(node.Expression), typeof(TTarget).GetProperty(node.Member.Name));
+        }
+        return base.VisitMember(node);
+    }
+
+}
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+       
+
+        List<Patient> patientsFromDb = new List<Patient>
+        {
+            new()
+            {
+                Name = "Magic",
+                Id = 4,
+            },
+            new()
+            {
+                Name="Johnson",
+                Id= 5,
+            }
+        };
+
+
+        List<Student> studentsFromDb = new List<Student>
+        {
+            new()
+            {
+               StudentId = new Guid("55ed2bc6-05be-4345-a726-347a21e13f7d"),
+               ClassName = "Test"
+            },
+            new()
+            {
+               StudentId = new Guid("ff8726c9-5d76-427d-a20d-843a7afff3fe"),
+               ClassName = "Test2"
+            }
+
+        };
+
+
+
+        Expression<Func<PatientDto, bool>> lambdaExpression = patient => patient.Id == 4 && patient.Name == "Magic";
+
+        var modifiedExpression = new LamdbaExpressionConverter<PatientDto, Patient>().Convert(lambdaExpression);
+        //var modifiedExpression = (Expression<Func<Patient,bool>>)lambdaVisitor.Modify(lambdaExpression);
+        var result = patientsFromDb.Where(modifiedExpression.Compile()).FirstOrDefault().Name;
+        Console.WriteLine(result);
+
+        Expression<Func<StudentDto, bool>> anotherLambdaExpression = student => student.ClassName == "Test";
+
+        var modifiedExpression2 =  new LamdbaExpressionConverter<StudentDto, Student>().Convert(anotherLambdaExpression);
+
+        var result2 = studentsFromDb.Where(modifiedExpression2.Compile()).FirstOrDefault().StudentId;
+        Console.WriteLine(result2);
+
+    }
+}
+
